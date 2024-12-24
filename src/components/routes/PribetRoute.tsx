9b931083d1ca821a50802a704f3e840c266483d4
Joy Das <joy600508@gmail.com>
2024-12-25 00:23:22 +0600

    update code

--- src/components/routes/PribetRoute.tsx
+++ src/components/routes/PribetRoute.tsx
@@ -24,7 +24,7 @@ const PribetRoute = ({ children }: { children: React.ReactNode }) => {
         <div className="flex justify-center flex-col items-center">
           <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-green-500 mb-4"></div>
           <h1 className="text-lg">
-            Authenticating
+            Loading
             <span className="text-2xl font-semibold">
               <span className="dot-animation">.</span>
               <span className="dot-animation">.</span>
